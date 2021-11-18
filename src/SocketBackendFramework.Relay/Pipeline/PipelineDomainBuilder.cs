c2da40f6167ed19a07c1e8977b9a46b126a110e3
Banyc <36535895+Banyc@users.noreply.github.com>
2021-11-18 20:44:06 +0800

    wip

--- src/SocketBackendFramework.Relay/Pipeline/PipelineDomainBuilder.cs
+++ src/SocketBackendFramework.Relay/Pipeline/PipelineDomainBuilder.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using SocketBackendFramework.Relay.ContextAdaptor;
 using SocketBackendFramework.Relay.Models.Pipeline;
 using SocketBackendFramework.Relay.Transport;
+using SocketBackendFramework.Relay.Transport.Clients.SocketHandlers;
+using SocketBackendFramework.Relay.Transport.Listeners.SocketHandlers;
 
 namespace SocketBackendFramework.Relay.Pipeline
 {
@@ -16,12 +19,14 @@ namespace SocketBackendFramework.Relay.Pipeline
         public abstract PipelineDomain<TMiddlewareContext> Build();
 
         protected PipelineDomain<TMiddlewareContext> Build(Pipeline<TMiddlewareContext> pipeline,
-                                                           IContextAdaptor<TMiddlewareContext> contextAdaptor)
+                                                           IContextAdaptor<TMiddlewareContext> contextAdaptor,
+                                                           Dictionary<string, IServerHandlerBuilder> serverHandlerBuilders,
+                                                           Dictionary<string, IClientHandlerBuilder> clientHandlerBuilders)
         {
             ConfigurateMiddlewares(pipeline);
 
             TransportMapper<TMiddlewareContext> TransportMapper =
-                new(config.TransportMapper, pipeline, contextAdaptor);
+                new(config.TransportMapper, serverHandlerBuilders, clientHandlerBuilders, pipeline, contextAdaptor);
 
             PipelineDomain<TMiddlewareContext> pipelineDomain = new(
                 this.config,
