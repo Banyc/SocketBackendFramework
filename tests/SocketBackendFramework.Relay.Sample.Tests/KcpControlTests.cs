064d833d6b6a6448805e9c1223a0914866a6333c
Banyc <36535895+Banyc@users.noreply.github.com>
2021-11-23 20:48:59 +0800

    wip

--- tests/SocketBackendFramework.Relay.Sample.Tests/KcpControlTests.cs
+++ tests/SocketBackendFramework.Relay.Sample.Tests/KcpControlTests.cs
@@ -16,23 +16,23 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
             byte[] bigBuffer = new byte[1024 * 1024 * 10];
             int writtenDataSize;
             Span<byte> bufferSpan = bigBuffer.AsSpan();
-            KcpControl kcpControl_1 = new KcpControl(0x1, false, 0);
-            KcpControl kcpControl_2 = new KcpControl(0x1, false, 0);
+            KcpControl kcpControl_1 = new KcpControl(0x1, isStreamMode: false, receiveWindowSize: 0);
+            KcpControl kcpControl_2 = new KcpControl(0x1, isStreamMode: false, receiveWindowSize: 0);
 
             string applicationString = "hello world";
             byte[] applicationBytes = System.Text.Encoding.UTF8.GetBytes(applicationString);
 
-            // send application bytes
+            // kcpControl_1 sends application bytes
             kcpControl_1.Send(applicationBytes);
             writtenDataSize = kcpControl_1.Output(bufferSpan);
             kcpControl_2.Input(bufferSpan[..writtenDataSize]);
 
-            // ack
+            // kcpControl_2 acks
             writtenDataSize = kcpControl_2.Output(bufferSpan);
             Assert.Equal((int)KcpSegment.DataOffset, writtenDataSize);
             kcpControl_1.Input(bufferSpan[..writtenDataSize]);
 
-            // get application bytes
+            // kcpControl_2 gets application bytes
             byte[] receivedApplicationBytes = new byte[1024 * 1024 * 10];
             int receivedApplicationByteSize = kcpControl_2.Receive(receivedApplicationBytes);
             string receivedApplicationString = System.Text.Encoding.UTF8.GetString(receivedApplicationBytes.AsSpan()[..receivedApplicationByteSize]);
@@ -47,35 +47,36 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
             byte[] bigBuffer = new byte[1024 * 1024 * 10];
             int writtenDataSize;
             Span<byte> bufferSpan = bigBuffer.AsSpan();
-            KcpControl kcpControl_1 = new KcpControl(0x1, false, 3);
-            KcpControl kcpControl_2 = new KcpControl(0x1, false, 3);
+            KcpControl kcpControl_1 = new KcpControl(0x1, isStreamMode: false, receiveWindowSize: 3);
+            KcpControl kcpControl_2 = new KcpControl(0x1, isStreamMode: false, receiveWindowSize: 3);
 
+            // it requires kcpControl_1 to send three segments
             byte[] applicationBytes = new byte[kcpControl_1.Mtu * 2];
             random.NextBytes(applicationBytes);
 
-            // send application bytes
+            // kcpControl_1 sends application bytes
             kcpControl_1.Send(applicationBytes);
             writtenDataSize = kcpControl_1.Output(bufferSpan);
             kcpControl_2.Input(bufferSpan[..writtenDataSize]);
 
-            // ack
-            // let kcpControl_2 update the window size for kcpControl_1
+            // kcpControl_2 acks
+            // let kcpControl_2 update the window size for kcpControl_1 so that kcpControl_1 can send more data
             writtenDataSize = kcpControl_2.Output(bufferSpan);
             Assert.Equal((int)KcpSegment.DataOffset * 1, writtenDataSize);
             kcpControl_1.Input(bufferSpan[..writtenDataSize]);
 
-            // send the rest of the application bytes
+            // kcpControl_1 sends the rest of the application bytes
             writtenDataSize = kcpControl_1.Output(bufferSpan);
             kcpControl_2.Input(bufferSpan[..writtenDataSize]);
             writtenDataSize = kcpControl_1.Output(bufferSpan);
             kcpControl_2.Input(bufferSpan[..writtenDataSize]);
 
-            // ack
+            // kcpControl_2 acks
             writtenDataSize = kcpControl_2.Output(bufferSpan);
             Assert.Equal((int)KcpSegment.DataOffset * 2, writtenDataSize);
             kcpControl_1.Input(bufferSpan[..writtenDataSize]);
 
-            // get application bytes
+            // kcpControl_2 gets application bytes
             byte[] receivedApplicationBytes = new byte[1024 * 1024 * 10];
             int receivedApplicationByteSize = kcpControl_2.Receive(receivedApplicationBytes);
 
