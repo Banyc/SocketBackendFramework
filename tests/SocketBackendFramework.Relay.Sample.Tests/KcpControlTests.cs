f5097f7e6d95016e039e4165a952c5bbfb38123c
Banyc <36535895+Banyc@users.noreply.github.com>
2021-12-14 20:21:21 +0800

    feat(kcp): two-way TX test case

--- tests/SocketBackendFramework.Relay.Sample.Tests/KcpControlTests.cs
+++ tests/SocketBackendFramework.Relay.Sample.Tests/KcpControlTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using SocketBackendFramework.Relay.Sample.Workflows.DefaultWorkflow.DefaultPipelineDomain.Protocols.Kcp;
 using SocketBackendFramework.Relay.Sample.Workflows.DefaultWorkflow.DefaultPipelineDomain.Protocols.Kcp.Models;
@@ -78,8 +80,8 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
                     }
                     // Task.Run(() =>
                     // {
-                        System.Diagnostics.Debug.WriteLine($"kcpControl_1.TryingOutput: {writtenDataSize} bytes");
-                        kcpControl_2.Input(bigBuffer.AsSpan()[..writtenDataSize]);
+                    System.Diagnostics.Debug.WriteLine($"kcpControl_1.TryingOutput: {writtenDataSize} bytes");
+                    kcpControl_2.Input(bigBuffer.AsSpan()[..writtenDataSize]);
                     // });
                 }
             };
@@ -97,8 +99,8 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
                     }
                     // Task.Run(() =>
                     // {
-                        System.Diagnostics.Debug.WriteLine($"kcpControl_2.TryingOutput: {writtenDataSize} bytes");
-                        kcpControl_1.Input(bigBuffer.AsSpan()[..writtenDataSize]);
+                    System.Diagnostics.Debug.WriteLine($"kcpControl_2.TryingOutput: {writtenDataSize} bytes");
+                    kcpControl_1.Input(bigBuffer.AsSpan()[..writtenDataSize]);
                     // });
                 }
             };
@@ -273,8 +275,8 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
                     }
                     // Task.Run(() =>
                     // {
-                        System.Diagnostics.Debug.WriteLine($"kcpControl_1.TryingOutput: {writtenDataSize} bytes");
-                        kcpControl_2.Input(bigBuffer.AsSpan()[..writtenDataSize]);
+                    System.Diagnostics.Debug.WriteLine($"kcpControl_1.TryingOutput: {writtenDataSize} bytes");
+                    kcpControl_2.Input(bigBuffer.AsSpan()[..writtenDataSize]);
                     // });
                 }
             };
@@ -292,8 +294,8 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
                     }
                     // Task.Run(() =>
                     // {
-                        System.Diagnostics.Debug.WriteLine($"kcpControl_2.TryingOutput: {writtenDataSize} bytes");
-                        kcpControl_1.Input(bigBuffer.AsSpan()[..writtenDataSize]);
+                    System.Diagnostics.Debug.WriteLine($"kcpControl_2.TryingOutput: {writtenDataSize} bytes");
+                    kcpControl_1.Input(bigBuffer.AsSpan()[..writtenDataSize]);
                     // });
                 }
             };
@@ -435,6 +437,183 @@ namespace tests.SocketBackendFramework.Relay.Sample.Tests
             lock (applicationBytesQueue)
             {
                 Assert.True(applicationBytesQueue.Count == 0);
+            }
+        }
+
+        [Fact]
+        public async Task ApplicationBytesAreABitLongWithMoreEventHandlingAndPacketLossGoBothWaysAsync()
+        {
+            Random random = new Random();
+            KcpConfig config = new()
+            {
+                ConversationId = (uint)Guid.NewGuid().GetHashCode(),
+                IsStreamMode = false,
+                ReceiveWindowSize = 3,
+                ShouldSendSmallPacketsNoDelay = false,
+                RetransmissionTimeout = TimeSpan.FromMilliseconds(10),
+                OutputDuration = TimeSpan.FromMilliseconds(10),
+            };
+            using KcpControl kcpControl_1 = new KcpControl(config);
+            using KcpControl kcpControl_2 = new KcpControl(config);
+
+            Queue<byte[]> applicationBytesQueue1 = new Queue<byte[]>();
+            Queue<byte[]> applicationBytesQueue2 = new Queue<byte[]>();
+
+            TaskCreationOptions taskCreationOptions = TaskCreationOptions.RunContinuationsAsynchronously;
+            TaskCompletionSource<object?> kcpControl1ReceiveTask = new(taskCreationOptions);
+            TaskCompletionSource<object?> kcpControl2ReceiveTask = new(taskCreationOptions);
+
+            kcpControl_1.TryingOutput += (sender, e) =>
+            {
+                Console.WriteLine($"kcpControl_1.TryingOutput");
+                while (true)
+                {
+                    byte[] bigBuffer = new byte[kcpControl_1.Mtu];
+                    Span<byte> bufferSpan = bigBuffer.AsSpan();
+                    // must be declared in the loop so that the value in the new thread will not be altered
+                    int writtenDataSize = kcpControl_1.Output(bufferSpan);
+                    if (writtenDataSize <= 0)
+                    {
+                        break;
+                    }
+                    if (random.Next(0, 100) < 10)
+                    {
+                        // oops, packet loss
+                        Console.WriteLine($"kcpControl_1.TryingOutput: {writtenDataSize} bytes lost");
+                        continue;
+                    }
+                    System.Diagnostics.Debug.WriteLine($"kcpControl_1.TryingOutput: {writtenDataSize} bytes");
+                    kcpControl_2.Input(bigBuffer.AsSpan()[..writtenDataSize]);
+                }
+            };
+            kcpControl_2.TryingOutput += (sender, e) =>
+            {
+                Console.WriteLine($"kcpControl_2.TryingOutput");
+                while (true)
+                {
+                    byte[] bigBuffer = new byte[kcpControl_2.Mtu];
+                    Span<byte> bufferSpan = bigBuffer.AsSpan();
+                    int writtenDataSize = kcpControl_2.Output(bufferSpan);
+                    if (writtenDataSize <= 0)
+                    {
+                        break;
+                    }
+                    if (random.Next(0, 100) < 10)
+                    {
+                        // oops, packet loss
+                        Console.WriteLine($"kcpControl_2.TryingOutput: {writtenDataSize} bytes lost");
+                        continue;
+                    }
+                    System.Diagnostics.Debug.WriteLine($"kcpControl_2.TryingOutput: {writtenDataSize} bytes");
+                    kcpControl_1.Input(bigBuffer.AsSpan()[..writtenDataSize]);
+                }
+            };
+            kcpControl_1.ReceivedCompleteSegment += (sender, completeSegmentBatchCount) =>
+            {
+                Console.WriteLine($"kcpControl_1.ReceivedCompleteSegment");
+                int i;
+                for (i = 0; i < completeSegmentBatchCount; i++)
+                {
+                    byte[] receivedApplicationBytes = new byte[1024 * 1024 * 10];
+                    int receivedApplicationByteSize;
+                    receivedApplicationByteSize = kcpControl_1.Receive(receivedApplicationBytes);
+                    byte[] previousSent;
+                    bool isApplicationBytesQueue2Empty = false;
+                    lock (applicationBytesQueue2)
+                    {
+                        previousSent = applicationBytesQueue2.Dequeue();
+                        isApplicationBytesQueue2Empty = applicationBytesQueue2.Count == 0;
+                    }
+                    try
+                    {
+                        Assert.True(previousSent.AsSpan().SequenceEqual(receivedApplicationBytes.AsSpan()[..receivedApplicationByteSize]));
+                    }
+                    catch (Exception e)
+                    {
+                        kcpControl1ReceiveTask.SetException(e);
+                        throw;
+                    }
+                    if (isApplicationBytesQueue2Empty)
+                    {
+                        kcpControl1ReceiveTask.SetResult(null);
+                    }
+                }
+            };
+            kcpControl_2.ReceivedCompleteSegment += (sender, completeSegmentBatchCount) =>
+            {
+                Console.WriteLine($"kcpControl_2.ReceivedCompleteSegment");
+                int i;
+                for (i = 0; i < completeSegmentBatchCount; i++)
+                {
+                    byte[] receivedApplicationBytes = new byte[1024 * 1024 * 10];
+                    int receivedApplicationByteSize;
+                    receivedApplicationByteSize = kcpControl_2.Receive(receivedApplicationBytes);
+                    byte[] previousSent;
+                    bool isApplicationBytesQueue1Empty = false;
+                    lock (applicationBytesQueue1)
+                    {
+                        previousSent = applicationBytesQueue1.Dequeue();
+                        isApplicationBytesQueue1Empty = applicationBytesQueue1.Count == 0;
+                    }
+                    try
+                    {
+                        Assert.True(previousSent.AsSpan().SequenceEqual(receivedApplicationBytes.AsSpan()[..receivedApplicationByteSize]));
+                    }
+                    catch (Exception e)
+                    {
+                        kcpControl2ReceiveTask.SetException(e);
+                        throw;
+                    }
+                    if (isApplicationBytesQueue1Empty)
+                    {
+                        kcpControl2ReceiveTask.SetResult(null);
+                    }
+                }
+            };
+
+            // activate stopwatch
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            // kcpControl_1 sends application bytes
+            Task kcpControl1SendTask = Task.Run(() =>
+            {
+                for (int i = 0; i < 300; i++)
+                {
+                    byte[] applicationBytes = new byte[kcpControl_1.Mtu * 2];
+                    random.NextBytes(applicationBytes);
+                    lock (applicationBytesQueue1)
+                    {
+                        applicationBytesQueue1.Enqueue(applicationBytes);
+                    }
+                    kcpControl_1.Send(applicationBytes);
+                }
+            });
+            // kcpControl_2 sends application bytes
+            Task kcpControl2SendTask = Task.Run(() =>
+            {
+                for (int i = 0; i < 300; i++)
+                {
+                    byte[] applicationBytes = new byte[kcpControl_2.Mtu * 2];
+                    random.NextBytes(applicationBytes);
+                    lock (applicationBytesQueue2)
+                    {
+                        applicationBytesQueue2.Enqueue(applicationBytes);
+                    }
+                    kcpControl_2.Send(applicationBytes);
+                }
+            });
+
+            await Task.WhenAll(kcpControl1SendTask, kcpControl2SendTask, kcpControl1ReceiveTask.Task, kcpControl2ReceiveTask.Task);
+            stopwatch.Stop();
+            System.Diagnostics.Debug.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+
+            lock (applicationBytesQueue1)
+            {
+                Assert.True(applicationBytesQueue1.Count == 0);
+            }
+            lock (applicationBytesQueue2)
+            {
+                Assert.True(applicationBytesQueue2.Count == 0);
             }
         }
     }
